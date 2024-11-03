167c5baebdeb64dc0768f0c4fa6b704fabe785a0
Tarique Anwer <itzmetariq@gmail.com>
2024-11-03 13:48:32 +0530

    configure logger

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,6 +1,9 @@
 import logging.config
 from fastapi import FastAPI
 
+# setup loggers
+logging.config.fileConfig('weather_app/config/logging.conf', disable_existing_loggers=False)
+
 # get root logger
 logger = logging.getLogger('weather_app')
 
