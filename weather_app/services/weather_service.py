4283bec77a18ec316e8c96b365bb3f3d15c5a4ca
Tarique Anwer <itzmetariq@gmail.com>
2024-11-04 11:13:07 +0530

    added event_logger to weather_service

--- weather_app/services/weather_service.py
+++ weather_app/services/weather_service.py
@@ -1,12 +1,15 @@
 from weather_app.services.weather_api import OpenWeatherFetcher
 from weather_app.services.storage import StorageService, S3StorageService
-from weather_app.config.constants import S3_BUCKET
+from weather_app.services.event_logger import EventLogger, DynamoEventLogger
+from weather_app.config.constants import S3_BUCKET, DYNAMODB_TABLE
+import time
 
 
 class WeatherService:
-    def __init__(self, fetcher: OpenWeatherFetcher, storage: StorageService):
+    def __init__(self, fetcher: OpenWeatherFetcher, storage: StorageService, logger: EventLogger):
         self.fetcher = fetcher
         self.storage = storage
+        self.logger = logger
 
     async def get_weather(self, city: str) -> dict:
         """
@@ -17,11 +20,13 @@ class WeatherService:
 
         weather_data = await self.fetcher.fetch_weather(city)
         storage_url = await self.storage.save_to_storage(city, weather_data)
+        await self.logger.log_event(city, int(time.time()), storage_url)
         return weather_data
 
 
 # Instantiate dependencies
 fetcher = OpenWeatherFetcher()
 storage = S3StorageService(S3_BUCKET)
+event_logger = DynamoEventLogger(DYNAMODB_TABLE)
 
-weather_service = WeatherService(fetcher, storage)
+weather_service = WeatherService(fetcher, storage, event_logger)
