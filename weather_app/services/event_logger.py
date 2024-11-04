e336d59606c33ebbaf229da3f88ee978c1175ece
Tarique Anwer <itzmetariq@gmail.com>
2024-11-04 12:16:55 +0530

    added EventId as partition key for dynamodb table

--- weather_app/services/event_logger.py
+++ weather_app/services/event_logger.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import aioboto3
 import logging
 from weather_app.config.constants import AWS_REGION
+import uuid
 
 
 class EventLogger(ABC):
@@ -18,13 +19,16 @@ class DynamoEventLogger(EventLogger):
 
     async def log_event(self, city: str, timestamp: int, storage_url: str):
         logging.info(f"Storing Event in dynamodb {self.dynamodb_table} table.")
-        logging.debug(f"city={city}, timestamp={timestamp}, S3_url={storage_url}")
+        # Generate a unique EventId
+        event_id = str(uuid.uuid4())
+        logging.debug(f"EventId={event_id}, city={city}, timestamp={timestamp}, S3_url={storage_url}")
         async with aioboto3.Session().client('dynamodb', region_name=AWS_REGION) as dynamodb:
             await dynamodb.put_item(
                 TableName=self.dynamodb_table,
                 Item={
-                    'city': city,
-                    'timestamp': str(timestamp),
-                    'S3_url': storage_url
+                    'EventId': {'S': event_id},  # Partition key
+                    'city': {'S': city},
+                    'timestamp': {'N': str(timestamp)},
+                    'storage_url': {'S': storage_url}
                 }
             )
