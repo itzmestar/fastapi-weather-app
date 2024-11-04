03600df0c038822830630548529b3f460351d5d0
Tarique Anwer <itzmetariq@gmail.com>
2024-11-04 12:51:27 +0530

    implemented caching

--- weather_app/services/weather_service.py
+++ weather_app/services/weather_service.py
@@ -2,6 +2,7 @@ from weather_app.services.weather_api import OpenWeatherFetcher
 from weather_app.services.storage import StorageService, S3StorageService
 from weather_app.services.event_logger import EventLogger, DynamoEventLogger
 from weather_app.config.constants import S3_BUCKET, DYNAMODB_TABLE
+from weather_app.services.cache_service import get_cached_weather
 import time
 
 
@@ -17,9 +18,16 @@ class WeatherService:
         :param city: city name
         :return: weather data
         """
+        # check caches weather data if available then return the same
+        cached_weather_data = await get_cached_weather(city, self.storage)
+        if cached_weather_data:
+            return cached_weather_data
 
+        # fetch weather data
         weather_data = await self.fetcher.fetch_weather(city)
+        # save to S3 bucket
         storage_url = await self.storage.save_to_storage(city, weather_data)
+        # log the event in Dynamodb
         await self.logger.log_event(city, int(time.time()), storage_url)
         return weather_data
 
